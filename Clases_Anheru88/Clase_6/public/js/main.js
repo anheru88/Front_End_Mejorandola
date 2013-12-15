897a392136fb52b81c3f32a9fa6e4599bdfc09f4
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2013-12-15 18:14:53 -0500

    Archivos Base al momento de comenzar la clase.

---
+++ Clases_Anheru88/Clase_6/public/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	console.log('Starting app');
+
+	window.collections.articles = new Puls3.Collections.ArticlesCollection();
+
+	window.collections.articles.on('add', function(model){
+		var view = new Puls3.Views.ArticleView(model);
+
+		view.render();
+
+		view.$el.appendTo("#contenido");
+	});
+
+	window.routers = new Puls3.Routers.BaseRouter();
+
+	var xhr = $.get('/articles/all');
+
+	xhr.done(function(data){
+		console.log(data);
+
+		data.forEach(function(articles){
+			window.collections.articles.add(articles);
+		});
+
+		Backbone.history.start({
+			root : "/",
+			pushState : true,
+			silent : false
+		});
+	});
+
+	$('nav li:first').on('click', function(){
+		Backbone.history.navigate('', {trigger: true});
+	});
+
+});
