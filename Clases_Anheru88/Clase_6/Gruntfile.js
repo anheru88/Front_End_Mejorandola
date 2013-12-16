e0abd23717cb98d666fd81b488edf336ccc6401f
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2013-12-16 00:20:50 -0500

    Modificacion del grunt file para el uglify

--- Clases_Anheru88/Clase_6/Gruntfile.js
+++ Clases_Anheru88/Clase_6/Gruntfile.js
@@ -15,9 +15,31 @@ module.exports = function(grunt) {
 			collection:{},
 			model:{},
 			template:{}
+		},
+		uglify : {
+			options : {
+				compress : true,
+				report : true,
+				banner : '/*!<%= grunt.template.date() %> */\n'
+			},
+			app : {
+				files : {
+					'public/app.min.js' : [
+						'public/js/init.js',
+						'public/js/backbone/models/article.js',
+						'public/js/backbone/collections/articles.js',
+						'public/js/backbone/routers/base.js',
+						'public/js/backbone/views/article.js',
+						'public/js/backbone/views/articleNew.js',
+						'public/js/main.js'
+					]
+				}
+			}
 		}
 	});
 
+	
 
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-bb-generate');
 };
