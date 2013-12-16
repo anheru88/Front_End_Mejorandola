f0d2397108e5c8972d2426bb6891f2536b2c89e0
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2013-12-16 00:17:13 -0500

    Vista de New Articulo....

--- Clases_Anheru88/Clase_6/public/js/backbone/views/articleNew.js
+++ Clases_Anheru88/Clase_6/public/js/backbone/views/articleNew.js
@@ -5,35 +5,34 @@ Puls3.Views.ArticleNewView = Backbone.View.extend({
 	className:"",
 	initialize : function($el){
 		this.$el = $el;
-		// this.template = _.template($("#ArticleNew_tpl").html());
+		//this.template = _.template($("#ArticleNew_tpl").html());
 	},
 	create : function(){
-		console.log('Button fue clickeado');
+		console.log("Boton Clickeado");
 
-		var title = this.$el.find('#title').val();
-		var tag = this.$el.find('#tag').val();
-		var contenido = this.$el.find('#content').val();
+		var title =  this.$el.find('#title').val();
+		var tag =  this.$el.find('#tag').val();
+		var content =  this.$el.find('#content').val();
 
-		console.log(title, tag, contenido);
-
-		var model = new Puls3.Models.Article({
-			title : title,
-			tag : tag,
-			content : contenido
+		var model = new Puls3.Models.Article({ 
+			title   : title,
+			tag     : tag, 
+			content : content
 		});
 
 		var xhr = model.save();
 
 		xhr.done(function(data){
-			Backbone.history.navigate('article/'+data.id, {trigger: true});
+			console.log(data);
+			//Backbone.history.navigate('article/' + data.id, {trigger : true});
 		});
 	},
 	render: function(data) {
-		// var self = this;
-		// var locals ={};
+	/*	var self = this;
+		var locals ={};
 
-		// this.$el.html(this.template({data:locals}));
+		this.$el.html(this.template({data:locals}));
 
-		// return this;
+		return this;*/
 	}
 });
